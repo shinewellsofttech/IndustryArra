1712f67c5eafcd8e420fbb87b4d62791f7a9e8c8
kuljeet007 <kuljeetsingh01588@gmail.com>
2026-07-08 11:17:58 +0530

    Restore local icon library stylesheet imports and local CSS references to resolve blank icons

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 import ThemeContext from "./context/ThemeContext";
 
-// Flaticon fonts (used in some parts of the app)
+// Import Icons Globally to prevent empty boxes/missing icons
+import "./icons/font-awesome/css/all.min.css";
 import "./icons/flaticon/flaticon.css";
 import "./icons/flaticon_1/flaticon_1.css";
+import "./icons/line-awesome/css/line-awesome.min.css";
+import "./icons/simple-line-icons/css/simple-line-icons.css";
+import "./icons/themify-icons/css/themify-icons.css";
 
 // Inject global style
 const fontStyle = document.createElement('style');
